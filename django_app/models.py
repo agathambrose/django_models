33d67e6f5be2272a5dbb4faefe1d2ff0afd3f471
Agatha Ambrose <agathambrose@gmail.com>
2022-04-13 10:32:12 +0000

    Models created for the users

---
+++ django_app/models.py
@@ -0,0 +1,13 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    photo_url = models.TextField(
+        max_length=200, default='', null=True, blank=True)
+    date_created = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
